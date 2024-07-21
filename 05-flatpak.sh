5e3c753ec56dc29e185bcfb86c188525bd0d6773
Koleffel <kolefell@proton.me>
2024-07-21 15:48:23 +0300

    Adding the Flatpak installation ,updating the WineHQ for Fedora 40

---
+++ 05-flatpak.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+###########################################################
+##########  Created by KOLEFELL ###########################
+########## e-mail : kolefell@proton.me ####################
+###########################################################
+
+# KDENLIVE
+
+flatpak install flathub org.kde.kdenlive -y
+
+#obs-studio
+
+flatpak install flathub com.obsproject.Studio -y
+
+#qbittorrent
+
+flatpak install flathub org.qbittorrent.qBittorrent -y
+
+#onlyoffice
+
+flatpak install flathub org.onlyoffice.desktopeditors -y
+
+#install VIBER
+
+#flatpak install flathub com.viber.Viber -y
+
+#install WAREHOUSE - manage FLATPAKs
+
+#flatpak install flathub io.github.flattool.Warehouse -y
+
+#VSCode
+#flatpak install flathub com.visualstudio.code -y
+
+#Thunderbird Mail client
+flatpak install flathub org.mozilla.Thunderbird -y
+
+#Audacity
+flatpak install flathub org.audacityteam.Audacity -y
+
+#Skype
+#flatpak install flathub com.skype.Client -y
+
+#EasyEffects
+#flatpak install flathub com.github.wwmm.easyeffects -y
+
+#YouTube Downloader
+flatpak install flathub io.github.aandrew_me.ytdn -y
+
+#Popsicle
+flatpak install flathub com.system76.Popsicle -y
+
+#spotify
+
+#flatpak install flathub com.spotify.Client -y
+
+#telegram
+
+#flatpak install flathub org.telegram.desktop -y
+
+########### GAMING ##################
+
+#Steam
+flatpak install flathub com.valvesoftware.Steam -y
+
+#Lutris
+flatpak install flathub net.lutris.Lutris -y
+
+#Heroic Games Launcher
+flatpak install flathub com.heroicgameslauncher.hgl -y
+
+### MINION for ESO and WOW addons
+
+flatpak install flathub gg.minion.Minion -y
+
+### ProtonUp-QT - for easy install of proton-GE
+
+flatpak install flathub net.davidotek.pupgui2 -y
+
+#Discord
+flatpak install flathub com.discordapp.Discord -y
+
+#install FLATSEAL - for flatpak permissions
+
+flatpak install flathub com.github.tchx84.Flatseal -y
+
+########## BROWSERS ############
+
+#install BRAVE
+
+#flatpak install flathub com.brave.Browser -y
+
+#install FLOORP
+
+flatpak install flathub one.ablaze.floorp -y
+
+#Microsoft EDGE Browser
+#flatpak install flathub com.microsoft.Edge -y
+
+#Google Chrome Browser
+#flatpak install flathub com.google.Chrome -y
+
+#Chromium Browser
+#flatpak install flathub org.chromium.Chromium -y
+
+#Vivaldi Browser
+#flatpak install flathub com.vivaldi.Vivaldi -y
+
+#Opera Browser
+#flatpak install flathub com.opera.Opera -y
+
+#update All the FLATPAKS
+
+flatpak update -y
