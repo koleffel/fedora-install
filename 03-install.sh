f818ecf1a39443a2ff8382bafb319b594034ec78
Koleffel <kolefell@proton.me>
2024-07-21 15:50:45 +0300

    modified:   00-current.sh 	modified:   03-install.sh 	modified:   04-gaming.sh

--- 03-install.sh
+++ 03-install.sh
@@ -16,69 +16,6 @@ sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x
 # Update dnf and install Sublime Text:
 sudo dnf install sublime-text
 
-####### FLATHUB ########
-
-#install Popsicle
-
-flatpak install flathub com.system76.Popsicle -y
-
-#install Blender
-
-flatpak install flathub org.blender.Blender -y
-
-#install Kdenlive
-
-flatpak install flathub org.kde.kdenlive -y
-
-#install Obs-studio
-
-flatpak install flathub com.obsproject.Studio -y
-
-#install Onlyoffice
-
-flatpak install flathub org.onlyoffice.desktopeditors -y
-
-#install Brave Browser
-
-flatpak install flathub com.brave.Browser -y
-
-#install qBittorrent
-
-flatpak install flathub org.qbittorrent.qBittorrent -y
-
-#install Gimp
-
-flatpak install flathub org.gimp.GIMP -y
-
-#install Spotify
-
-flatpak install flathub com.spotify.Client -y
-
-#install Telegram
-
-flatpak install flathub org.telegram.desktop -y
-
-#install Viber
-
-flatpak install flathub com.viber.Viber -y
-
-#install Handbrake
-
-flatpak install flathub fr.handbrake.ghb -y
-
-#install VScode
-
-flatpak install flathub com.visualstudio.code -y
-
-#install Youtube Downloader Plus
-
-flatpak install flathub io.github.aandrew_me.ytdn -y
-
-#install Audacity
-
-flatpak install flathub org.audacityteam.Audacity -y
-
-
 ####### Install With DNF ##########
 
 # install VLC 
--- 04-gaming.sh
+++ 04-gaming.sh
@@ -7,7 +7,7 @@
 
 echo "Adding the WINEHQ Repo !!!"
 
-sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/38/winehq.repo
+sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/40/winehq.repo
 
 echo "Installing the WINE package !"
 
@@ -15,11 +15,11 @@ sudo dnf install winehq-staging -y
   
 echo "Installing the LUTRIS package !"
 
-sudo dnf install steam lutris  -y
+#sudo dnf install steam lutris  -y
 
 echo "Installing Steam "
 
-sudo dnf install steam -y
+#sudo dnf install steam -y
 
 echo " ######################################## "
 echo " #### Now You Are Ready To Game !!! ##### "
