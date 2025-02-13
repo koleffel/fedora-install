2a1c7b4dc9d7e5a51dbf49ce73eb468ab52ca8b7
Kolefell <57557101+koleffel@users.noreply.github.com>
2025-02-13 18:23:29 +0200

    Update 03-install.sh
    Adding extra packages - meson . obs-studio, kdenlive etc

--- 03-install.sh
+++ 03-install.sh
@@ -8,13 +8,13 @@
 
 ###### Installing SUBLIME ########
 # Install the GPG key:
-sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+#sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 
 # Adding The Stable Repo :
-sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+#sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 
 # Update dnf and install Sublime Text:
-sudo dnf install sublime-text
+#sudo dnf install sublime-text
 
 ####### Install With DNF ##########
 
@@ -28,7 +28,15 @@ sudo dnf install git -y
 
 # install simplescreenrecorder 
 
-sudo dnf install simplescreenrecorder -y
+#sudo dnf install simplescreenrecorder -y
+
+# Install MESON - Meson is a build system (similar to automake) which can generate code for other lower-level build systems. For example, it can generate code for ninja. When packaging software which builds using Meson it's important to use the %meson macros instead of %ninja or other lower-level build system macros directly.
+
+sudo dnf install meson -y
+
+# Installing some essential packages - Obs-studio , KDENLIVE , python3
+
+sudo dnf install obs-studio  kdenlive gimp  kate qbittorrent python3 -y
 
 
  
